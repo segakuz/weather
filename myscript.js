2a73460568f3c486fe501d1415a75a3cf4e765c7
segakuz <segakuz@gmail.com>
2019-01-16 15:42:49 +0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
 transliterate = (
   function() {
     var
-      rus = "щ   ш  ч  ц  ю  я  ё  е  ый ж  х  ъ  ы  э  а б в г д е з и й й к л м н о п р с т у ф х ь".split(/ +/g),
-      eng = "shh sh ch cz yu ya yo ye yy zh kh `` y' e` a b v g d e z i j y k l m n o p r s t u f h '".split(/ +/g)
+      rus = "щ    льск ш  ч  ц  ю  я  ё  е  ый ой ий ж  х  ц  ъ  ы э  а б в г д е з и й й к л м н о п р с т у ф х ь ь".split(/ +/g),
+      eng = "shch lsk  sh ch cz yu ya yo ye yy oy iy zh kh ts `` y e` a b v g d e z i j y k l m n o p r s t u f h ' ’".split(/ +/g)
     ;
     return function(text, engToRus) {
       text = text.toLowerCase();
